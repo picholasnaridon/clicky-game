594d9be79550e8635c67262b2e6f9d1442d3d8b5
nick paridon <paridonn1@gmail.com>
2018-09-19 20:01:37 -0400

    updates

--- src/components/Score.js
+++ src/components/Score.js
@@ -2,21 +2,12 @@ import React, { Component } from 'react';
 import { Label } from 'react-bootstrap';
 class Score extends Component {
 	render() {
-		if (!this.props.lost) {
-			return (
-				<h1>
-					Score: <Label bsStyle="primary">{this.props.score}</Label> | High Score:{' '}
-					<Label bsStyle="success"> {this.props.highScore}</Label>
-				</h1>
-			);
-		} else {
-			return (
-				<h1>
-					Score: <Label bsStyle="danger">{this.props.score}</Label> | High Score:{' '}
-					<Label bsStyle="danger"> {this.props.highScore}</Label>
-				</h1>
-			);
-		}
+		return (
+			<h1>
+				Score: <Label bsStyle={this.props.lost ? 'danger' : 'primary'}>{this.props.score}</Label> | High Score:{' '}
+				<Label bsStyle={this.props.lost ? 'danger' : 'success'}> {this.props.highScore}</Label>
+			</h1>
+		);
 	}
 }
 
